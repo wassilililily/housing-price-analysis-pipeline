3a60c1c3c7d6bd5c018a33bf282106de84b7d15d
ddeung0302 <100173663+ddeung0302@users.noreply.github.com>
2025-04-06 22:29:42 +0800

    URA ETL update

---
+++ dags/tasks/URA/load_ura.py
@@ -0,0 +1,54 @@
+from airflow.decorators import task
+import pandas as pd
+import psycopg2
+import os
+
+@task
+def load_ura(ura_csv):
+    df = pd.read_csv(ura_csv)
+    table_name = os.path.basename(ura_csv)
+    conn = psycopg2.connect(host="postgres", port=5432, dbname="airflow", user="airflow", password="airflow")
+    cur = conn.cursor()
+
+    create_query = f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            id SERIAL PRIMARY KEY,
+            project_name TEXT NOT NULL,
+            transacted_price NUMERIC NOT NULL,
+            area_sqft NUMERIC NOT NULL,
+            unit_price_psf NUMERIC NOT NULL,
+            sale_date DATE NOT NULL,
+            street_name TEXT,
+            type_of_sale TEXT,
+            type_of_area TEXT,
+            area_sqm NUMERIC,
+            unit_price_psm NUMERIC,
+            nett_price TEXT,
+            property_type TEXT,
+            number_of_units INTEGER,
+            tenure TEXT,
+            postal_district INTEGER NOT NULL,
+            market_segment TEXT,
+            floor_level TEXT
+            floor_level_continuous FLOAT,
+            remaining_lease_months INTEGER
+        );
+        """
+    cur.execute(create_query)
+    conn.commit()
+
+    for _, row in df.iterrows():
+        values = tuple(row.fillna('').astype(str))
+        insert_query = f"""
+            INSERT INTO {table_name} (
+                project_name, transacted_price, area_sqft, unit_price_psf, sale_date,
+                street_name, type_of_sale, type_of_area, area_sqm, unit_price_psm,
+                nett_price, property_type, number_of_units, tenure, postal_district,
+                market_segment, floor_level,floor_level_continuous, remaining_lease_months
+            ) VALUES ({','.join(['%s'] * 19)});
+            """
+        cur.execute(insert_query, values)
+        conn.commit()
+
+    cur.close()
+    conn.close()
