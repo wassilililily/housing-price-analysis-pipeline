70331fffb1795b3c41a459f00bc7afd55e051c15
su8729 <113164032+su8729@users.noreply.github.com>
2025-04-26 22:07:39 +0800

    Update linear.py

--- dags/tasks/models/linear.py
+++ dags/tasks/models/linear.py
@@ -72,7 +72,7 @@ def linear_models(data_file_path, test_file_path):
         ])
         categorical_transformer = Pipeline([
             ('imputer', SimpleImputer(strategy='most_frequent')),
-            ('onehot', OneHotEncoder(handle_unknown='ignore', sparse=False))
+            ('onehot', OneHotEncoder(handle_unknown='ignore', sparse_output=False))
         ])
         preprocessor = ColumnTransformer([
             ('num', numeric_transformer, numeric_features),
